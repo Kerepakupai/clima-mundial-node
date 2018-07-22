424907948d5e449713fc4b7d13078c924567a974
David Fuentes <ing.davidfuentes@outlook.com>
2018-07-22 11:19:38 -0400

    Refactorizacion Obtener Coordenadas

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
 const argv = require('./config/yargs').argv;
-const axios = require('./config/axios').axios;
 
-let decodeDesc = decodeURI(argv.direccion);
+const lugar = require('./lugar/lugar');
 
-let url1 = `https://mapsgoogleapis.com/maps/api/geocode/json?address=${ decodeDesc }`;
-let url2 = `https://geocoder.cit.api.here.com/6.2/geocode.json?app_id=fAGGuxuiHyC8ZriA2lt2&app_code=YvotZXbwZceihaBzYWMscQ&searchtext=${ decodeDesc }`;
-
-axios.get(url1)
-    .then(response => {
-        /* Parametros de URL1
-            let locationResp = response.data.Response.View[0].Result[0].Location.NavigationPosition[0];
-            let addressResp = response.data.Response.View[0].Result[0].Location.Address;
-
-            let latitud = locationResp.Latitude;
-            let longitud = locationResp.Longitude;
-
-            let address = addressResp.Label;
-        */
-
-        let coors = response.data.results[0].geometry.location;
-        let direccion = response.data.results[0].formatted_address;
-
-        console.log(`LATITUD: ${ coors.lat }`);
-        console.log(`LONGITUD: ${ coors.lng }`);
-        console.log(`DIRECCION: ${ direccion }`);
-    })
-    .catch(error => console.log('ERROR!!!!', error.code));
+lugar.getLugarLatLng(argv.direccion)
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
---
+++ lugar/lugar.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+// let url1 = `https://mapsgoogleapis.com/maps/api/geocode/json?address=${ decodeDesc }`;
+// let url2 = `https://geocoder.cit.api.here.com/6.2/geocode.json?app_id=fAGGuxuiHyC8ZriA2lt2&app_code=YvotZXbwZceihaBzYWMscQ&searchtext=${ decodeDesc }`;
+
+
+const getLugarLatLng = async(direccion) => {
+
+    let decodeDir = decodeURI(direccion);
+
+    try {
+        let resp = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ decodeDir}`);
+
+        if (resp.data.status === 'ZERO_RESULTS') {
+            throw new Error(`No se encontraron resultados para la direccion ${ direccion }`);
+        }
+
+        let coors = resp.data.results[0].geometry.location;
+        let location = resp.data.results[0];
+
+        return {
+            direccion: location.formatted_address,
+            lat: coors.lat,
+            lng: coors.lng
+        };
+
+    } catch (error) {
+        throw new Error('No se pudieron obtener los datos. Revise URL o Conexion a Internet');
+    }
+}
+
+module.exports = {
+    getLugarLatLng
+};
